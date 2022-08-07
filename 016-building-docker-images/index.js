e2240e9bb4cc3686d39cf75ade5298d79bff3e0a
alessandro-marchino <alessandro.marchino@csi.it>
2022-08-07 21:06:02 +0200

    Lesson 20

---
+++ 016-building-docker-images/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (_, res) => res.send('Hello World!'));
+const server = app.listen(3000, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log(`Example app listening at http://${host}:${port}`);
+});
+
